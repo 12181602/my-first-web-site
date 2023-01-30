ff784c711ffc17025069f7551c9860b240b22da0
12181602 <115630330+12181602@users.noreply.github.com>
2023-01-30 22:35:14 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const title = document.querySelector(".hello h1");
 console.dir(title);
 function handleTitleClick() {
-    title.style.color = "blue";
+    const currentColor = title.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    title.style.color = newColor;
 }
 function handleMouseEvent() {
     title.innerHTML = "Mouse is here!";
@@ -9,6 +16,27 @@ function handleMouseEvent() {
 function handleMouseLeave() {
     title.innerHTML = "Mouse is gone!";
 }
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEvent);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+    alert("copier!");
+}
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+function handleWindowOnline() {
+    alert("All good");
+}
+//title.addEventListener("click", handleTitleClick);
+//title.addEventListener("mouseenter", handleMouseEvent);
+//title.addEventListener("mouseleave", handleMouseLeave);
+
+title.onclick = handleTitleClick;
+title.onmouseenter = handleMouseEvent;
+title.onmouseleave = handleMouseLeave;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
