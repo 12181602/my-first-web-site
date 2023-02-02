90a774075fe8c0bd132ff4c82c985b39026659d2
12181602 <115630330+12181602@users.noreply.github.com>
2023-02-02 16:57:19 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,31 @@
-const title = document.querySelector(".hello h1");
-console.dir(title);
-function handleTitleClick() {
-    const currentColor = title.style.color;
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "tomato";
-    } else {
-        newColor = "blue";
-    }
-    title.style.color = newColor;
-}
-/*
-function handleMouseEvent() {
-    title.innerHTML = "Mouse is here!";
-}
-function handleMouseLeave() {
-    title.innerHTML = "Mouse is gone!";
-}
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-    alert("copier!");
-}
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-function handleWindowOnline() {
-    alert("All good");
-}
-//title.addEventListener("click", handleTitleClick);
-//title.addEventListener("mouseenter", handleMouseEvent);
-//title.addEventListener("mouseleave", handleMouseLeave);
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-title.onclick = handleTitleClick;
-title.onmouseenter = handleMouseEvent;
-title.onmouseleave = handleMouseLeave;
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
-*/
-
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick() {
-    /*
-    const clickedClass = "active";
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
-    */
-    h1.classList.toggle("active");
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+   // greeting.innerHTML = "Hello " + username;
+    paintGreetings(username);
 }
 
-h1.addEventListener("click", handleTitleClick);
+function paintGreetings(username) {
+    greeting.innerHTML = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the greetings
+    paintGreetings(savedUsername);
+}
