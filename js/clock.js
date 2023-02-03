72647195f3e290ddab5d08d7fbd0501a060eb46f
12181602 <115630330+12181602@users.noreply.github.com>
2023-02-03 15:30:33 +0900

    Add files via upload

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+//setInterval(sayHello, 5000); //5초에 한번씩 함수 실행
+
+//setTimeout(sayHello, 5000); //5초 뒤에 한번 수행
+
+getClock();
+setInterval(getClock, 1000);
